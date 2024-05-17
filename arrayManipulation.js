152bbe6b0b1de5bef156ecb820c99569e1258638
dem0saic <vowusu032@st.ug.edu.gh>
2024-05-17 12:28:18 +0000

    Write a function processArray that: - Takes an array of numbers as an argument. - Returns a new array where each even number is squared and each odd number is tripled.

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -23,27 +23,3 @@ function processArray(array) {
     }
     return arrayOfNUmbers;
 }
-
-function formatArrayStrings(strigs, arrayOfNUmbers){
-
-    // Creating a new array to store the processed results
-    if(strigs.length !== arrayOfNUmbers.length){
-
-        // Throwing an error if the arrays are not of the same length
-        throw new Error ("Arrays must have the same length");
-        
-    }
-
-    // Check if the array is empty or contains duplicates and remove duplicates from the array itself 
-    for(let i = 0; i < strigs.length; i ++){
-        const string = strigs[i];
-        const number = arrayOfNUmbers[i];
-         
-        // check if the string contains a number that matches the string length  instead of returning null  
-        if(number % 2 == 0){
-            strigs[i] = string + " squared";
-        }else {
-            strigs[i] = string + " tripled";
-        }
-    }
-}
